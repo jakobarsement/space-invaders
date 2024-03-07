a2b17823374fc5c0dfd0e0db08d9b2322aae1844
Jakob Arsement <jakobarsement@gmail.com>
2024-03-07 17:54:07 -0600

    Create the game loop using the crossterm library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use std::{error::Error, io, time::Duration};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Setup the terminal, which serves as the game UI
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    'gameloop: loop {
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => break 'gameloop,
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    // Cleanup
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    return Ok(());
 }
